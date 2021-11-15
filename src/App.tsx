d957b1e3345ab6497b9f4e29e8748d473ceeb74f
Lucas Maso <lucasmaso@hotmail.com>
2021-11-15 17:04:18 -0300

    Aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
 
 function App() {
-  
-
   return (
     <BrowserRouter>
       <AuthContextProvider>
@@ -16,6 +15,8 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/rooms/new" element={<NewRoom/>} />
           <Route path="/rooms/:id" element={<Room/>} />
+
+          <Route path="/admin/rooms/:id" element={<AdminRoom/>} />
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
